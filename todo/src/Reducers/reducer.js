4f67bb921f1e604215821247e11787bccc9a4c9a
Your Name <you@example.com>
2019-08-13 17:34:15 -0400

    got tasks as an array

--- todo/src/Reducers/reducer.js
+++ todo/src/Reducers/reducer.js
@@ -1,20 +1,21 @@
-export const initialState = {
-    todos: [{
+export const initialState = 
+    [{
     item: 'Learn about reducers',
     completed: false,
     id: 3892987589
 }]
-}
+
 
 
 
 export const Reducer = (state, action) => {
     switch(action.type) {
         case 'ADD_TASK':
-            return {
-                ...state,
-                item: action.payload,
-                id: Date.now()
+            return { 
+                todos: [...state,
+                {item: action.payload,
+                id: Date.now(),
+                completed: false}]
             }
 
         default:
